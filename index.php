1bef7819b0ec2d9a76d94b6abe00bab30d4d3719
lucpmatos <lucpmatos@gmail.com>
2018-04-08 14:48:13 -0300

    comit inicial

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>2A Soluções</title>
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <div class="total">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <p>teste</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
