4de468ce61f5c93dda39eeea0fdf687e15b49c90
lucpmatos <lucpmatos@gmail.com>
2018-04-08 16:08:02 -0300

    pagina de apresentacao

--- index.php
+++ index.php
@@ -7,13 +7,27 @@
     <title>2A Soluções</title>
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
     <link rel="stylesheet" href="css/estilo.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/solid.js" integrity="sha384-P4tSluxIpPk9wNy8WSD8wJDvA8YZIkC6AQ+BfAFLXcUZIPQGu4Ifv4Kqq+i2XzrM" crossorigin="anonymous"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/fontawesome.js" integrity="sha384-2IUdwouOFWauLdwTuAyHeMMRFfeyy4vqYNjodih+28v2ReC+8j+sLF9cK339k5hY" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="total">
         <div class="container">
             <div class="row">
-                <div class="col-xs-12">
-                    <p>teste</p>
+                <div class="col-12">
+                    <img src="img/logo-2A-03.png" alt="Logo 2A Soluções">
+                </div>
+                <div class="col-12">
+                    <hr>
+                </div>
+                <div class="col-12">
+                    <div class="conteudo">
+                        <h1>Representante oficial:</h1>
+                        <img src="img/lumilight.png" alt="Logo Lumilight">
+                        <p>Contato:</p>
+                        <p class="phone"><i class="fas fa-phone"></i> +55 21 98015-2221 | +55 21 98271-3737</p>
+                        <p class="email"><i class="fas fa-envelope"></i> andreluis@2asolucoes.com.br | andrecamargo@2asolucoes.com.br</p>
+                    </div>
                 </div>
             </div>
         </div>
